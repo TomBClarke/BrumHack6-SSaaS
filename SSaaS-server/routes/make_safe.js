225d54d39d5b09e50f78f6d2321f43d240a0bc6c
Tom Clarke <tomclarke96@hotmail.co.uk>
2017-03-25 16:31:06 +0000

    Added VS to gitignore and made makesafe return HTTP 200

--- SSaaS-server/routes/make_safe.js
+++ SSaaS-server/routes/make_safe.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* POST make safe */
 router.get('/', function(req, res, next) {
-  res.status(401).send({'hi': 'there'});
+  res.status(200).send({'hi': 'there'});
 });
 
 module.exports = router;
