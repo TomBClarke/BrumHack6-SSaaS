48fa5b3549356588fb02b94f54859519aaaa734e
Tom Clarke <tomclarke96@hotmail.co.uk>
2017-03-26 00:22:02 +0000

    Extension 'should' work for text.
    Needs testing.

--- SSaaS-chrome-extension/background.js
+++ SSaaS-chrome-extension/background.js
@@ -1,16 +1,100 @@
-chrome.storage.local.get(["settings"], function (value) {
-  console.log(value);
-  // TODO - Do something with the settings.
+var DEFAULTS = {
+  'active': true,
+  'settings': {
+    'emotion': 0.6,
+    'language': 0.6,
+    'social': 0.6
+  }
+},
+  currentSettings = DEFAULTS,
+  SSAAS_URL = '',
+  SSAAS_TEXT = 'makesafe',
+  SSAAS_IMG = 'makeimgsafe';
+
+// Failure for any issue.
+function failure(data) {
+  console.log("SSaaS Extension Error");
+  console.log(data);
+}
+
+// Loads initial data.
+chrome.storage.local.get(["active", "settings"], function (value) {
+  // Load values.
+  currentSettings.active = value.active;
+  currentSettings.settings = value.settings;
+
+  // Initial check.
+  if (currentSettings.active) {
+    updatePageText();
+  }
 });
 
 chrome.runtime.onMessage.addListener(onMsg);
 
-/**
- * On a message sent from popup.js.
- * @param data
- */
+// Handles a message from popup.js
 function onMsg(data) {
-  if (data.settings) {
-    chrome.storage.local.set({"settings": data.settings});
+  switch (data.action) {
+    case "HANDSHAKE":
+      chrome.runtime.sendMessage(currentSettings);
+      break;
+    case "TOGGLE_ACTIVE":
+      currentSettings.active = !currentSettings.active;
+      saveData();
+      break;
+    case "UPDATE_DATA":
+      currentSettings.settings = data.settings;
+      saveData();
+      break;
   }
+}
+
+// Saves data to chrome storage.
+function saveData() {
+  chrome.storage.local.set({ "active": currentSettings.active, "settings": currentSettings.settings });
+}
+
+// Get list of text nodes.
+function textNodesUnder(node) {
+  var all = [];
+  if (node.tagName === undefined || node.tagName.toLowerCase() === 'script') {
+    return all;
+  }
+  for (node = node.firstChild; node; node = node.nextSibling) {
+    // Text fields
+    if (node.nodeType == 3) {
+      // Only get text node with text.
+      if (/[a-zA-Z]/.test($(node).text())) {
+        all.push(node);
+      }
+    } else {
+      all = all.concat(textNodesUnder(node));
+    }
+  }
+  return all;
+}
+
+// Calls the SSaaS.
+function processSentences(element) {
+  $.ajax({
+    type: "POST",
+    url: SSAAS_URL + '/' + SSAAS_TEXT,
+    data: { 
+      'text': $(element).text(),
+      'emotion': currentSettings.settings.emotion,
+      'language': currentSettings.settings.language,
+      'social': currentSettings.settings.social
+    },
+    success: function (data) {
+      $(element).text(data.text);
+    },
+    failure: failure,
+    dataType: 'json'
+  });
+}
+
+// Check the page and updates the page's TEXT.
+function updatePageText() {
+  $(textNodesUnder(document.body)).each(function (index, element) {
+    processSentences(element);
+  });
 }
--- SSaaS-chrome-extension/popup.js
+++ SSaaS-chrome-extension/popup.js
@@ -1,20 +1,81 @@
-document.getElementById('submitButton').onclick = function() {
-	var settings = document.getElementById("settings").value;
-  sendToCurrentTab({
-    settings: {
-      emotion: 0.6,
-      language: 0.6,
-      social: 0.6
+$(function () {
+  // Initiate data.
+  sendToCurrentTab({ 'action': "HANDSHAKE" });
+
+  chrome.runtime.onMessage.addListener(function (currentSettings) {
+    // Display initial values.
+    $('#emotion').val(currentSettings.settings.emotion);
+    $('#language').val(currentSettings.settings.language);
+    $('#social').val(currentSettings.settings.social);
+
+    if (!currentSettings.active) {
+      toggleActiveStatusVisually();
     }
   });
-};
 
-document.getElementById('toggleActive').onclick = function() {
-  sendToCurrentTab({ activeToggle: true });
-};
+  // Toggle active status of extension.
+  $('#toggleActive').click(function () {
+    toggleActiveStatusVisually();
+    sendToCurrentTab({ 'action': 'TOGGLE_ACTIVE' });
+  });
 
-function sendToCurrentTab(data) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, data);
+  // Save configuration of tones.
+  $('#saveSettings').click(function () {
+    $('#error').text("");
+
+    var settings = {
+      'emotion': parseFloat($('#emotion').val()),
+      'language': parseFloat($('#language').val()),
+      'social': parseFloat($('#social').val())
+    }
+
+    try {
+      validateNumber(settings.emotion);
+      validateNumber(settings.language);
+      validateNumber(settings.social);
+
+      // All validated.
+      sendToCurrentTab({ 'action': 'UPDATE_DATA', 'settings': settings });
+    } catch (error) {
+      // One didn't pass validation.
+      $('#error').text(error.message);
+    }
+  });
+
+  // Validate the number.
+  function validateNumber(num) {
+    if (isNaN(num)) {
+      throw new Error('number ' + num + ' is \'NaN\' :\'(');
+    }
+    if (num > 1) {
+      throw new Error('number ' + num + ' is too large :\'(');
+    }
+    if (num < 0) {
+      throw new Error('number ' + num + ' is too small :\'(');
+    }
+  }
+
+  // Active/disable fields.
+  function toggleActiveStatusVisually() {
+    if ($('#toggleActive').text() == 'Turn Off') {
+      $('#toggleActive').text('Turn On');
+      $('#emotion').prop('disabled', true);
+      $('#language').prop('disabled', true);
+      $('#social').prop('disabled', true);
+      $('#saveSettings').prop('disabled', true);
+    } else {
+      $('#toggleActive').text('Turn Off');
+      $('#emotion').prop('disabled', false);
+      $('#language').prop('disabled', false);
+      $('#social').prop('disabled', false);
+      $('#saveSettings').prop('disabled', false);
+    }
+  }
+
+  // Send data to background script.
+  function sendToCurrentTab(data) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, data);
     });
-}
+  }
+});
